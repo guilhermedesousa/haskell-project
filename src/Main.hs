479f63df49c6086c07799babaea014999d849297
Guilherme Santos <guilhermedesousa.dev@gmail.com>
2024-08-11 00:56:09 -0300

    feat: update board state

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,13 @@
 module Main (main) where
 
 import Board
+import Player
+import Piece
 
 main :: IO ()
 main = do
-  let board = createInitialBoard
-  printBoard board
+  let initialBoard = createInitialBoard
+  let dummyCell = createCell A Peao
+  let newBoard = updateCell (2, 2) (3, 2) dummyCell initialBoard
+  printBoard initialBoard
+  printBoard newBoard
